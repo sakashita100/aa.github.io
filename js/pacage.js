9f9cd66e847ec0f94b87d3f744699fa7ddf28940
sakashita100 <116332404+sakashita100@users.noreply.github.com>
2024-06-26 15:06:33 +0900

    Create pacage.js

---
+++ js/pacage.js
@@ -0,0 +1,12 @@
+{
+  "name": "my-mqtt-project",
+  "version": "1.0.0",
+  "description": "A simple MQTT project",
+  "main": "mqtt_publisher.js",
+  "scripts": {
+    "start": "node mqtt_publisher.js"
+  },
+  "dependencies": {
+    "mqtt": "^4.2.6"
+  }
+}
