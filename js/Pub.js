f4f19d58b52e8d3736842f6cc29b45359e1f588e
sakashita100 <116332404+sakashita100@users.noreply.github.com>
2024-06-26 15:08:09 +0900

    Delete js/Pub.js

--- js/Pub.js
+++
@@ -1,8 +0,0 @@
-const mqtt = require('mqtt');
-const client  = mqtt.connect('mqtt://localhost'); // ブローカーのURL
-
-client.on('connect', () => {
-  client.publish('test/topic', 'Hello MQTT');
-  console.log('Message published');
-  client.end();
-});
